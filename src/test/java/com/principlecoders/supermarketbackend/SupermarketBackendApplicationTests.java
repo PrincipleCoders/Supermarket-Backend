5d7a2738d034cad07a92ef39b250171c076c9838
ChethiyaNuwan <115537042+ChethiyaNuwan@users.noreply.github.com>
2023-11-03 15:25:26 +0530

    initialized spring boot project

---
+++ src/test/java/com/principlecoders/supermarketbackend/SupermarketBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.principlecoders.supermarketbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupermarketBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
