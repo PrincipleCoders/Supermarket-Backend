2a37e0fa75ce0849d97709d72d9c05c020e579ac
ChethiyaNuwan <115537042+ChethiyaNuwan@users.noreply.github.com>
2023-11-09 01:07:00 +0530

    created service ports template

---
+++ common/src/main/java/com/principlecoders/common/utils/ServicePorts.java
@@ -0,0 +1,9 @@
+package com.principlecoders.common.utils;
+
+public interface ServicePorts {
+    int API_GATEWAY_PORT = 8080;
+    int USER_PORT = 8085;
+    int DELIVERY_PORT = 8086;
+    int ORDER_PORT = 8087;
+    int INVENTORY_PORT = 8088;
+}
