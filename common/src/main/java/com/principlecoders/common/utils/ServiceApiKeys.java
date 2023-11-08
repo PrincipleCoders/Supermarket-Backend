7ef15eb090bc4039b85a1be533dd90d2ca9d1286
ChethiyaNuwan <115537042+ChethiyaNuwan@users.noreply.github.com>
2023-11-09 01:13:14 +0530

    created service api keys template

---
+++ common/src/main/java/com/principlecoders/common/utils/ServiceApiKeys.java
@@ -0,0 +1,8 @@
+package com.principlecoders.common.utils;
+
+public interface ServiceApiKeys {
+    String USER_API_KEY = "USER-KEY-123";
+    String ORDER_API_KEY = "ORDER-KEY-234";
+    String INVENTORY_API_KEY = "INVENTORY-KEY-345";
+    String DELIVERY_API_KEY = "DELIVERY-KEY-456";
+}
