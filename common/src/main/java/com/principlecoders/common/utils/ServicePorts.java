0a3b8d4a1b687ebe05b44310c939f0753bbb1c29
ChethiyaNuwan <115537042+ChethiyaNuwan@users.noreply.github.com>
2023-11-09 11:31:22 +0530

    converted service ports to service urls

--- common/src/main/java/com/principlecoders/common/utils/ServicePorts.java
+++
@@ -1,9 +0,0 @@
-package com.principlecoders.common.utils;
-
-public interface ServicePorts {
-    int API_GATEWAY_PORT = 8081;
-    int USER_PORT = 8085;
-    int DELIVERY_PORT = 8086;
-    int ORDER_PORT = 8087;
-    int INVENTORY_PORT = 8088;
-}
---
+++ common/src/main/java/com/principlecoders/common/utils/ServiceUrls.java
@@ -0,0 +1,9 @@
+package com.principlecoders.common.utils;
+
+public interface ServiceUrls {
+    String API_GATEWAY_URL = "http://localhost:8081/";
+    String USER_URL = "http://localhost:8085/";
+    String DELIVERY_URL = "http://localhost:8086/";
+    String ORDER_URL = "http://localhost:8087/";
+    String INVENTORY_URL = "http://localhost:8088/";
+}
