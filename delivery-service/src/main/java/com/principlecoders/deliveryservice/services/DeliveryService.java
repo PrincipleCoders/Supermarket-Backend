18b7d15e7644d3e13f6572be64c1528408894280
Harshana Batagalla <harshanabatagalla@gmail.com>
2023-11-10 23:42:03 +0530

    Created Delivery service

---
+++ delivery-service/src/main/java/com/principlecoders/deliveryservice/services/DeliveryService.java
@@ -0,0 +1,14 @@
+package com.principlecoders.deliveryservice.services;
+
+import com.principlecoders.deliveryservice.DeliveryServiceApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = "com.principlecoders")
+public class DeliveryService {
+    public static void main(String[] args) {
+        SpringApplication.run(DeliveryServiceApplication.class, args);
+    }
+}
