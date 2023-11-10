08565bcf9c2b96fa63076e7b97cb4aeea5b9fa11
ChethiyaNuwan <115537042+ChethiyaNuwan@users.noreply.github.com>
2023-11-10 17:59:51 +0530

    added web client config

---
+++ common/src/main/java/com/principlecoders/common/configs/WebClientConfig.java
@@ -0,0 +1,20 @@
+package com.principlecoders.common.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfig {
+    @Bean
+    public WebClient initWebClient() {
+        return WebClient.builder()
+                .defaultHeader("Content-Type", "application/json")
+                .defaultHeader("Accept", "application/json")
+                .filter((request, next) -> {
+                    System.out.println("Request: " + request.method() + " " + request.url() + " " + request.headers());
+                    return next.exchange(request);
+                })
+                .build();
+    }
+}
