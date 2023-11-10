b7ced2dd18bb7a3f768d1eef9f76aad14f1fb41b
ChethiyaNuwan <115537042+ChethiyaNuwan@users.noreply.github.com>
2023-11-10 18:01:55 +0530

    added component scan

--- api-gateway/src/main/java/com/principlecoders/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/principlecoders/apigateway/ApiGatewayApplication.java
@@ -2,8 +2,10 @@ package com.principlecoders.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.principlecoders"})
 public class ApiGatewayApplication {
 
     public static void main(String[] args) {
