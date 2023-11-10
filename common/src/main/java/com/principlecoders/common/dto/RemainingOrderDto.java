eefad2ce0a315655f48fe834c2da5c693fdf8d2e
SandeepaniGunasekara <115693602+SandeepaniGunasekara@users.noreply.github.com>
2023-11-11 03:52:41 +0530

    remaining order dto

--- common/src/main/java/com/principlecoders/common/dto/RemainingOrderDto.java
+++ common/src/main/java/com/principlecoders/common/dto/RemainingOrderDto.java
@@ -1,1 +1,21 @@
+package com.principlecoders.common.dto;
 
+import lombok.*;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
+public class RemainingOrderDto {
+    private String id;
+    private Date date;
+    private String userId;
+    private String productId;
+    private int quantity;
+    private boolean isPacked;
+
+}
