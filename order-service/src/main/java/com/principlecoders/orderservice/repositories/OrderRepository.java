5b3db03cb8d10294ea4730a5e5131e6f0348e1fb
SandeepaniGunasekara <115693602+SandeepaniGunasekara@users.noreply.github.com>
2023-11-11 12:41:14 +0530

    created OrderRepository

---
+++ order-service/src/main/java/com/principlecoders/orderservice/repositories/OrderRepository.java
@@ -0,0 +1,4 @@
+package com.principlecoders.orderservice.repositories;
+
+public interface OrderRepository {
+}
--- order-service/src/main/java/com/principlecoders/orderservice/services/OrderService.java
+++ order-service/src/main/java/com/principlecoders/orderservice/services/OrderService.java
@@ -79,5 +79,6 @@ public class OrderService {
 
     public ResponseEntity<?> getRemainingOrders() {
 
+        return null;
     }
 }
