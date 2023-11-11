5ea6ddffa35dcb9de1bb1f8aabedac412825d16c
ChethiyaNuwan <115537042+ChethiyaNuwan@users.noreply.github.com>
2023-11-11 13:50:11 +0530

    crated user controller

---
+++ api-gateway/src/main/java/com/principlecoders/apigateway/controllers/UserController.java
@@ -0,0 +1,12 @@
+package com.principlecoders.apigateway.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/user")
+public class UserController {
+
+}
