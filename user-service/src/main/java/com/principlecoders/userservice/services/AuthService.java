8e73458419eeb8efb62e69cc853092c11d405784
ChethiyaNuwan <115537042+ChethiyaNuwan@users.noreply.github.com>
2023-11-11 13:47:58 +0530

    implemented logic validate login and set user roles

---
+++ user-service/src/main/java/com/principlecoders/userservice/services/AuthService.java
@@ -0,0 +1,80 @@
+package com.principlecoders.userservice.services;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseToken;
+import com.google.firebase.auth.UserRecord;
+import com.principlecoders.common.dto.ResponseMessage;
+import com.principlecoders.common.utils.UserRoles;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+    private final FirebaseAuth firebaseAuth;
+    public ResponseEntity<?> validateLogin(String accessToken) {
+        try {
+            FirebaseToken decodedToken = firebaseAuth.verifyIdToken(accessToken);
+            String uid = decodedToken.getUid();
+            UserRecord userRecord = firebaseAuth.getUser(uid);
+            Object userRoles = userRecord.getCustomClaims().get("role");
+            if (userRoles == null) {
+                userRoles = setUserRole(uid, UserRoles.CUSTOMER).getHeaders().get("Role");
+            }
+            boolean emailVerified = userRecord.isEmailVerified();
+            if (!emailVerified){
+                firebaseAuth.generateEmailVerificationLink(userRecord.getEmail());
+            }
+            String newToken = firebaseAuth.createCustomToken(uid);
+
+            return ResponseEntity.status(HttpStatus.ACCEPTED)
+                    .header("Authorization", "Bearer "+newToken)
+                    .header("Role", String.valueOf(userRoles))
+                    .header("Email_Verified", String.valueOf(emailVerified))
+                    .body(
+                            ResponseMessage.builder()
+                                    .message("Login successful")
+                                    .build()
+                    );
+        }
+        catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                    .body(
+                            ResponseMessage.builder()
+                                    .message("Login failed")
+                                    .error(e.getMessage())
+                                    .build()
+                    );
+        }
+    }
+
+    public ResponseEntity<?> setUserRole(String uid, UserRoles role) {
+        try {
+            Map<String, Object> claims = new HashMap<>();
+            claims.put("Role", role);
+            firebaseAuth.setCustomUserClaims(uid, claims);
+
+            return ResponseEntity.status(HttpStatus.ACCEPTED)
+                    .header("Role", String.valueOf(role))
+                    .body(
+                            ResponseMessage.builder()
+                                    .message("Role set successful")
+                                    .build()
+                    );
+        }
+        catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body(
+                            ResponseMessage.builder()
+                                    .message("Role set failed")
+                                    .error(e.getMessage())
+                                    .build()
+                    );
+        }
+    }
+}
