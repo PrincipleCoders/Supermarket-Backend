531e2bffbd7723ce9e742fd56125e93e55df1954
ChethiyaNuwan <115537042+ChethiyaNuwan@users.noreply.github.com>
2023-11-12 03:08:08 +0530

    created endpoint for get all user

--- api-gateway/src/main/java/com/principlecoders/apigateway/controllers/UserController.java
+++ api-gateway/src/main/java/com/principlecoders/apigateway/controllers/UserController.java
@@ -1,12 +1,17 @@
 package com.principlecoders.apigateway.controllers;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/user")
+@RequestMapping("user")
 public class UserController {
+    @GetMapping("/all")
+    public String getAllUsers() {
+        return "All users";
+    }
 
 }
