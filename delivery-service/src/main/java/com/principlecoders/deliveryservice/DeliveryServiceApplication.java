dcb7086d4e79dedd7505d6a9e5bf784e8d2a388f
ChethiyaNuwan <115537042+ChethiyaNuwan@users.noreply.github.com>
2023-11-13 00:23:20 +0530

    added component scan

--- delivery-service/src/main/java/com/principlecoders/deliveryservice/DeliveryServiceApplication.java
+++ delivery-service/src/main/java/com/principlecoders/deliveryservice/DeliveryServiceApplication.java
@@ -2,8 +2,10 @@ package com.principlecoders.deliveryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.principlecoders"})
 public class DeliveryServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(DeliveryServiceApplication.class, args);
--- user-service/src/main/java/com/principlecoders/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/principlecoders/userservice/UserServiceApplication.java
@@ -2,8 +2,10 @@ package com.principlecoders.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.principlecoders"})
 public class UserServiceApplication {
 
     public static void main(String[] args) {
