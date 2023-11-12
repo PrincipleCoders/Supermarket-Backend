7cc4f87a0f6be3bbc1e7c1761c1e8cd9f749b24d
ChethiyaNuwan <115537042+ChethiyaNuwan@users.noreply.github.com>
2023-11-13 01:00:46 +0530

    disabled security

--- api-gateway/src/main/java/com/principlecoders/apigateway/configs/GatewaySecurityConfig.java
+++ api-gateway/src/main/java/com/principlecoders/apigateway/configs/GatewaySecurityConfig.java
@@ -1,36 +1,36 @@
-package com.principlecoders.apigateway.configs;
-
-import com.principlecoders.apigateway.filters.SecurityChainFilter;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
-
-@Configuration
-@EnableWebSecurity
-public class GatewaySecurityConfig {
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .addFilterBefore(new SecurityChainFilter(), BasicAuthenticationFilter.class)
-                .csrf().disable()
-                .httpBasic().disable()
-                .formLogin().disable()
-                .cors(cors-> cors
-                        .configurationSource(request -> {
-                            var corsConfig = new org.springframework.web.cors.CorsConfiguration();
-                            corsConfig.addAllowedOrigin("*");
-                            corsConfig.addAllowedHeader("*");
-                            corsConfig.addAllowedMethod("*");
-                            return corsConfig;
-                        })
-                )
-                .authorizeHttpRequests((authorize) -> authorize
-                        .anyRequest().permitAll()
-                );
-        return http.build();
-    }
-}
-
+//package com.principlecoders.apigateway.configs;
+//
+//import com.principlecoders.apigateway.filters.SecurityChainFilter;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.web.SecurityFilterChain;
+//import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class GatewaySecurityConfig {
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http
+//                .addFilterBefore(new SecurityChainFilter(), BasicAuthenticationFilter.class)
+//                .csrf().disable()
+//                .httpBasic().disable()
+//                .formLogin().disable()
+//                .cors(cors-> cors
+//                        .configurationSource(request -> {
+//                            var corsConfig = new org.springframework.web.cors.CorsConfiguration();
+//                            corsConfig.addAllowedOrigin("*");
+//                            corsConfig.addAllowedHeader("*");
+//                            corsConfig.addAllowedMethod("*");
+//                            return corsConfig;
+//                        })
+//                )
+//                .authorizeHttpRequests((authorize) -> authorize
+//                        .anyRequest().permitAll()
+//                );
+//        return http.build();
+//    }
+//}
+//
