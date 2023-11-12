b2851edafe9cd1f59ec811eafd0a361373cd4e52
ChethiyaNuwan <115537042+ChethiyaNuwan@users.noreply.github.com>
2023-11-12 10:58:16 +0530

    changed urls

--- common/src/main/java/com/principlecoders/common/utils/ServiceUrls.java
+++ common/src/main/java/com/principlecoders/common/utils/ServiceUrls.java
@@ -2,8 +2,8 @@ package com.principlecoders.common.utils;
 
 public interface ServiceUrls {
     String API_GATEWAY_URL = "http://localhost:8081/";
-    String USER_URL = "http://localhost:8085/";
-    String DELIVERY_URL = "http://localhost:8086/";
-    String ORDER_URL = "http://localhost:8087/";
-    String INVENTORY_URL = "http://localhost:8088/";
+    String USER_URL = "http://localhost:8085/user/";
+    String DELIVERY_URL = "http://localhost:8086/delivery/";
+    String ORDER_URL = "http://localhost:8087/order/";
+    String INVENTORY_URL = "http://localhost:8088/inventory/";
 }
